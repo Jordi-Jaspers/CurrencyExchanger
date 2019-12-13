8bbb0d8ed1d2c5daa7849e0a3f0980656c9c66d5
Jordi-Jaspers <34105935+Jordi-Jaspers@users.noreply.github.com>
2019-12-13 16:01:30 +0100

    minor change

--- flask/app.py
+++ flask/app.py
@@ -9,12 +9,34 @@ import os
 # Create Flask Application
 app = Flask(__name__)
 
-# Create Secret key for Flaskform
+# Database Variables
 SECRET_KEY = os.urandom(32)
+
+DATABASE_HOST   = '127.0.0.1'
+DATABASE_USER   = 'root'
+DATABASE_PASS    = 'root'
+DATABASE_PORT   = '3306'
+DATABASE_DB        = 'currencies'
+
+DATABASE_URI = ('mysql+mysqlconnector://' +
+DATABASE_USER + ':' +
+DATABASE_PASS + '@' +
+DATABASE_HOST + ':' +
+DATABASE_PORT + '/' +
+DATABASE_DB
+)
+
+# Create Secret key for Flaskform
 app.config['SECRET_KEY'] = SECRET_KEY
+app.config['MYSQL_HOST'] = DATABASE_HOST
+app.config['MYSQL_USER'] = DATABASE_USER
+app.config['MYSQL_PASSWORD'] = DATABASE_PASS
+app.config['MYSQL_PORT'] = DATABASE_PORT
+app.config['MYSQL_DB'] = DATABASE_DB
 
 # Configure the database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/currencies'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URI
 mysql = SQLAlchemy(app)   
 
 class Currency(mysql.Model):
